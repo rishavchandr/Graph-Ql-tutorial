9743eaaa029cc75e8fedc6abf311a1b759c12de6
rishavchandr <rishavchandra02@gmail.com>
2026-02-02 00:37:53 +0530

    refactor code

---
+++ src/graphQl/user/resolvers.ts
@@ -0,0 +1,10 @@
+const queries = {}
+
+
+const mutations = {
+    createUser: async(_: any,{}:{}) => {
+         return 'SuccessFull';
+    }
+}
+
+export const resolvers = {queries,mutations}
--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,19 @@
 import express from "express";
-import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@as-integrations/express5';
-import {prisma} from './lib/db'
+import createApolloGraphQlServer from './graphQl/index'
 
 const port = process.env.PORT || 8000
 const app = express()
-
 app.use(express.json())
 
 async function init() {
-    
-    const server = new ApolloServer({
-        typeDefs: `
-           type Query {
-               hello: String
-               say(name: String): String
-           }
-           
-           type Mutation {
-               createUser(firstName: String!,lastName: String!,email: String!,password: String!): Boolean
-           }
-        `,
-        resolvers: {
-            Query: {
-               hello: () => `Hello i am graphQl server`,
-               say: (_,{name}: {name: string}) => `helllo i am ${name}`
-            },
-
-            Mutation: {
-                createUser: async(_,
-                    {firstName,
-                    lastName,
-                    email,
-                    password}: 
-                    {firstName:string;
-                     lastName:string; 
-                     email:string;
-                     password:string}) => {
-                    
-                    await prisma.user.create({
-                        data: {
-                            email,
-                            password,
-                            firstName,
-                            lastName,
-                            salt: "random-salt"
-                        }
-                    })
-
-                   return true;
-                }
-            },
-        },
-    })
-
-    await server.start()
-
-    app.use('/graphql' , expressMiddleware(server))
+    const graphqlServer = await createApolloGraphQlServer()
+    app.use('/graphql' , expressMiddleware(graphqlServer))
 }
 
 init()
+
+
 
 app.get('/' , (req,res) =>{
     res.json({message: "App is working"})
