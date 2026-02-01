584f5bba5ccb6d7af6f98742fa864e26c487c3ed
rishavchandr <rishavchandra02@gmail.com>
2026-02-01 23:36:15 +0530

    prisma done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@as-integrations/express5';
+import {prisma} from './lib/db'
 
 const port = process.env.PORT || 8000
 const app = express()
@@ -15,12 +16,41 @@ async function init() {
                hello: String
                say(name: String): String
            }
+           
+           type Mutation {
+               createUser(firstName: String!,lastName: String!,email: String!,password: String!): Boolean
+           }
         `,
         resolvers: {
             Query: {
                hello: () => `Hello i am graphQl server`,
                say: (_,{name}: {name: string}) => `helllo i am ${name}`
-            }
+            },
+
+            Mutation: {
+                createUser: async(_,
+                    {firstName,
+                    lastName,
+                    email,
+                    password}: 
+                    {firstName:string;
+                     lastName:string; 
+                     email:string;
+                     password:string}) => {
+                    
+                    await prisma.user.create({
+                        data: {
+                            email,
+                            password,
+                            firstName,
+                            lastName,
+                            salt: "random-salt"
+                        }
+                    })
+
+                   return true;
+                }
+            },
         },
     })
 
